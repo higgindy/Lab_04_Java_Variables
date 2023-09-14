8abe41e902a8d48d327b6876b21b4a8a76e86d09
Dylan Higgins <higgindy@mail.uc.edu>
2023-09-14 14:53:50 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // int0perandA, int0perand8, intSum, intProduct, intDifference, intQuotient, intModulo
+       int int0perandA = 4;
+       int int0perandB = 7;
+
+       int intSum = 0;
+       int intProduct = 0;
+       int intDifference = 0;
+       int intQuotient = 0;
+       int intModulo = 0;
+
+       intSum = int0perandA + int0perandB;
+        System.out.println("The sum of " + int0perandA + " and " +int0perandB + " is " + intSum);
+       intProduct = int0perandA * int0perandB;
+        System.out.println("The product of " + int0perandA + " and " + int0perandB + " is " + intProduct);
+
+        // Do rest of int statements here
+
+        double doubleOperandA = 3.14;
+        double doubleOperandB = 3.50;
+        double doubleSum = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+
+        // Do the rest of the double statements here
+
+    }
+}
